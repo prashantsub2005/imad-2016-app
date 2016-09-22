1a412bbe4b9aa3f0a2e3cfaec932b9c4eaced158
prashantsub2005 <prashantsub2005@gmail.com>
2016-09-22 18:17:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/vulture.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'vulture.png'));
+app.get('/ui/Vulture.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Vulture.png'));
 });
 
 
