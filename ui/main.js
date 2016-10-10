5a0313c865924c55f13a5e193b76cfe7a1de5066
prashantsub2005 <prashantsub2005@gmail.com>
2016-10-10 18:12:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('vulture');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 25;
+    marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
 }
     
     
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
