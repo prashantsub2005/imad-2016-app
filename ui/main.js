a701349b2bbd9b5a65b535422947d3008a3efc69
prashantsub2005 <prashantsub2005@gmail.com>
2016-10-10 18:03:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'New Value';
 //Move the image
 var image = document.getElementById('vulture');
 img.onclick = function () {
-    img.style.marginleft ='120px';
+    img.style.marginLeft = '120px';
 };
