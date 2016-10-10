336a1366569344f386211ddae01cea1484fd7dcd
prashantsub2005 <prashantsub2005@gmail.com>
2016-10-10 17:59:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //Move the image
-var image = document.getElementById('img');
+var image = document.getElementById('vulture');
 img.onclick = function (){
     img.style.marginleft ='120px';
 };
