9017be3846b64ca78eccd8de8fa0d3c30a06ae39
prashantsub2005 <prashantsub2005@gmail.com>
2016-10-15 16:59:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,6 @@ button.onclick = function () {
 
 //Submit name
 
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
 };
@@ -54,6 +52,8 @@ submit.onclick = function() {
         //Not done yet
     };
     //Make the request
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
     request.open("GET", "http://prashantsub2005.imad.hasura-app.io/submit-name?name=" +name, true);
     request.send("null");  
     
