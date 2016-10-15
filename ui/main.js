026ee2fcebb9c159ec7e87876e7a013a066ac795
prashantsub2005 <prashantsub2005@gmail.com>
2016-10-15 18:31:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //Counter code
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 
 button.onclick = function () {
     
@@ -12,7 +12,7 @@ button.onclick = function () {
             //take some action
         if (request.status === 200) {
             var counter = request.responseText;
-            var span = document.getElementById("count");
+            var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
             
@@ -20,31 +20,31 @@ button.onclick = function () {
         //Not done yet
     };
     //Make the request
-    request.open("GET", "http://prashantsub2005.imad.hasura-app.io/counter", true);
-    request.send("null");
+    request.open('GET', 'http://prashantsub2005.imad.hasura-app.io/counter', true);
+    request.send('null');
 };
 
 //Submit name
 
-var submit = document.getElementById("submit_btn");
+var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
 };
       //Create request object
     var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             //take some action
         if (request.status === 200) {
          //Capture the list of names and render them as a list
     var names = request.responseText;
     names = JSON.parse(names);
-    var list = "";
+    var list = '';
     for (var i=0; i< names.length; i++) {
-        list += "<li>" + names[i] + "</li>";
+        list += '<li>' + names[i] + '</li>';
     }
-    var ul=document.getElementById("namelist");
+    var ul=document.getElementById('namelist');
     ul.innerHTML = list;    
         }
             
@@ -52,9 +52,10 @@ submit.onclick = function() {
         //Not done yet
     };
     //Make the request
-    var nameInput = document.getElementById("name");
+    var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open("GET", "http://prashantsub2005.imad.hasura-app.io/submit-name?name=" +name, true);
-    request.send("null");  
+    
+    request.open('GET', 'http://prashantsub2005.imad.hasura-app.io/submit-name?name=' +name, true);
+    request.send('null');  
     
     ;
